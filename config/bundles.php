ec540cb405668b74b6df5bf2d559ed447cb9efda
Valentin Saik <svbackend22@gmail.com>
2018-04-12 11:28:24 +0300

    VS_8 Translations files

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Fesor\RequestObject\Bundle\RequestObjectBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Translation\Bundle\TranslationBundle::class => ['all' => true],
 ];
