c9d34516a2feab9dc0459d23b7b79aa64d4651b9
Valentin Saik <svbackend22@gmail.com>
2018-04-19 16:41:41 +0300

    VS Swift Mailer

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
