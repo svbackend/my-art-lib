33889964a0c996bb382a35e3c0f842d7d634f661
Valentin <svbackend22@gmail.com>
2018-05-11 19:39:45 +0300

    VS WatchedMovieProcessor small clean up

--- src/Movies/EventListener/WatchedMovieProcessor.php
+++ src/Movies/EventListener/WatchedMovieProcessor.php
@@ -18,6 +18,8 @@ use Interop\Queue\PsrProcessor;
 use Enqueue\Client\TopicSubscriberInterface;
 use Psr\Log\LoggerInterface;
 
+// Looks like here some logic problem
+// todo construct entities by params from message here instead of create each of them with correct associations
 class WatchedMovieProcessor implements PsrProcessor, TopicSubscriberInterface
 {
     const ADD_WATCHED_MOVIE_TMDB = 'addWatchedMovieTMDB';
@@ -32,6 +34,8 @@ class WatchedMovieProcessor implements PsrProcessor, TopicSubscriberInterface
     }
 
     /**
+     * This method called when user or guest trying to add movie to own list of watched movies but we've not
+     *
      * @param PsrMessage $message
      * @param PsrContext $session
      * @return object|string
@@ -41,38 +45,23 @@ class WatchedMovieProcessor implements PsrProcessor, TopicSubscriberInterface
     {
         $watchedMovies = $message->getBody();
         $watchedMovies = unserialize($watchedMovies);
+        $validClasses = [UserWatchedMovie::class, GuestWatchedMovie::class];
 
         /** @var $watchedMovies UserWatchedMovie[]|GuestWatchedMovie[] */
         foreach ($watchedMovies as $watchedMovie) {
+            if (in_array(get_class($watchedMovie), $validClasses) === false) {
+                $this->logger->error('Unexpected behavior: $watchedMovie not in range of valid classes', [
+                    'actualClass' => get_class($watchedMovie)
+                ]);
+                continue;
+            }
+
             $movie = $watchedMovie->getMovie(); // Not saved movie
-            $genres = $movie->getGenres();
-            $movie->removeAllGenres(); // Because doctrine doesn't know about these genres due unserialization
-            foreach ($genres as $genre) {
-                /** @var $genreReference Genre */
-                $genreReference = $this->em->getReference(Genre::class, $genre->getId()); // so we need to re-add 'em
-                $movie->addGenre($genreReference);
-            }
-
-            // Recreate *WatchedMovie entity with new managed by doctrine associations
-            if ($watchedMovie instanceof UserWatchedMovie) {
-                /** @var $userReference User */
-                $userReference = $this->em->getReference(User::class, $watchedMovie->getUser()->getId());
-                $newWatchedMovie = new UserWatchedMovie($userReference, $movie, $watchedMovie->getVote(), $watchedMovie->getWatchedAt());
-            }
-
-            if ($watchedMovie instanceof GuestWatchedMovie) {
-                /** @var $guestSessionReference GuestSession */
-                $guestSessionReference = $this->em->getReference(GuestSession::class, $watchedMovie->getGuestSession()->getId());
-                $newWatchedMovie = new GuestWatchedMovie($guestSessionReference, $movie, $watchedMovie->getVote(), $watchedMovie->getWatchedAt());
-            }
+            $movie = $this->refreshGenresAssociations($movie);
+            $newWatchedMovie = $this->recreateWatchedMovie($watchedMovie, $movie);
 
             $this->em->persist($movie);
-
-            if (isset($newWatchedMovie)) {
-                $this->em->persist($newWatchedMovie);
-            } else {
-                $this->logger->error('Unexpected behavior: $newWatchedMovie not isset');
-            }
+            $this->em->persist($newWatchedMovie);
         }
 
         try {
@@ -84,6 +73,75 @@ class WatchedMovieProcessor implements PsrProcessor, TopicSubscriberInterface
         }
 
         return self::ACK;
+    }
+
+    /**
+     * @param WatchedMovie $watchedMovie
+     * @param Movie $movie
+     * @return WatchedMovie
+     * @throws \Doctrine\ORM\ORMException
+     */
+    private function recreateWatchedMovie(WatchedMovie $watchedMovie, Movie $movie): WatchedMovie
+    {
+        // Recreate *WatchedMovie entity with new managed by doctrine associations
+        if ($watchedMovie instanceof UserWatchedMovie) {
+            $newWatchedMovie = $this->recreateUserWatchedMovie($watchedMovie, $movie);
+        }
+
+        if ($watchedMovie instanceof GuestWatchedMovie) {
+            $newWatchedMovie = $this->recreateGuestWatchedMovie($watchedMovie, $movie);
+        }
+
+        if (!isset($newWatchedMovie)) {
+            throw new \LogicException('Watched movie not a valid child class of WatchedMovie');
+        }
+
+        return $newWatchedMovie;
+    }
+    
+    /**
+     * @param UserWatchedMovie $userWatchedMovie
+     * @param Movie $movie
+     * @return UserWatchedMovie
+     * @throws \Doctrine\ORM\ORMException|\Exception
+     */
+    private function recreateUserWatchedMovie(UserWatchedMovie $userWatchedMovie, Movie $movie): UserWatchedMovie
+    {
+        /** @var $userReference User */
+        $userReference = $this->em->getReference(User::class, $userWatchedMovie->getUser()->getId());
+        return new UserWatchedMovie($userReference, $movie, $userWatchedMovie->getVote(), $userWatchedMovie->getWatchedAt());
+    }
+
+    /**
+     * @param GuestWatchedMovie $guestWatchedMovie
+     * @param Movie $movie
+     * @return GuestWatchedMovie
+     * @throws \Doctrine\ORM\ORMException|\Exception
+     */
+    private function recreateGuestWatchedMovie(GuestWatchedMovie $guestWatchedMovie, Movie $movie): GuestWatchedMovie
+    {
+        /** @var $guestSessionReference GuestSession */
+        $guestSessionReference = $this->em->getReference(GuestSession::class, $guestWatchedMovie->getGuestSession()->getId());
+        return new GuestWatchedMovie($guestSessionReference, $movie, $guestWatchedMovie->getVote(), $guestWatchedMovie->getWatchedAt());
+    }
+    
+    /**
+     * @param Movie $movie
+     * @return Movie
+     * @throws \Doctrine\ORM\ORMException
+     */
+    private function refreshGenresAssociations(Movie $movie): Movie
+    {
+        $genres = $movie->getGenres();
+        $movie->removeAllGenres(); // Because doctrine doesn't know about these genres due unserialization
+
+        foreach ($genres as $genre) {
+            /** @var $genreReference Genre */
+            $genreReference = $this->em->getReference(Genre::class, $genre->getId()); // so we need to re-add 'em
+            $movie->addGenre($genreReference);
+        }
+
+        return $movie;
     }
 
     public static function getSubscribedTopics()
