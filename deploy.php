4f2455925424859405c8211bbddbc7231fccda4e
Valentin <svbackend22@gmail.com>
2018-08-31 18:06:40 +0300

    Deployer (init)

---
+++ deploy.php
@@ -0,0 +1,42 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Project name
+set('application', 'mykino.top');
+
+// Project repository
+set('repository', 'git@github.com:svbackend/my-art-lib.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('159.89.14.82')
+    ->user('svbackend')
+    ->identityFile('~/.ssh/dokey')
+    ->set('deploy_path', '~/{{application}}');    
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+//before('deploy:symlink', 'database:migrate');
+
