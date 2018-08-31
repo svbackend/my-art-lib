2a8e9e3edafcb70420eb5fb467a6a369990ce5dd
Valentin <svbackend22@gmail.com>
2018-08-31 20:20:20 +0300

    Use old console version

--- deploy.php
+++ deploy.php
@@ -25,17 +25,9 @@ host('159.89.14.82')
     ->identityFile('~/.ssh/dokey')
     ->set('deploy_path', '~/{{application}}');
 
-set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --ignore-platform-reqs --no-scripts');
-
-task('deploy:dotenv', function() {
-    $environment = run('cat {{deploy_path}}/shared/.env');
-    $dotenv = new \Symfony\Component\Dotenv\Dotenv();
-    $dotenv->populate($dotenv->parse($environment));
-})->desc('Load DotEnv values');
-after('deploy:shared', 'deploy:dotenv');
+set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --ignore-platform-reqs --no-scripts');
 
 // Tasks
-
 task('build', function () {
     run('cd {{release_path}} && build');
 });
@@ -44,13 +36,23 @@ task('deploy:cache:clear', function () {
     run('{{bin/console}} cache:clear --no-warmup --env=prod');
 });
 
-
 task('deploy:cache:warmup', function () {
     run('{{bin/console}} cache:warmup --env=prod');
 });
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
+
+task('deploy:docker', function () {
+    run('cd {{release_path}} && docker-compose down');
+    run('cd {{release_path}} && docker-compose up -d');
+});
+before('deploy:symlink', 'deploy:docker');
+
+task('deploy:db', function () {
+    run('cd {{release_path}} && docker-compose exec app php bin/console doctrine:migrations:migrate');
+});
+after('deploy:docker', 'deploy:db');
 
 // Migrate database before symlink new release.
 
