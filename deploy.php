bd47c6606ff1a36430f32e33a5aa40118f0181e1
Valentin <svbackend22@gmail.com>
2018-08-31 19:32:37 +0300

    Add deployer as dev dependency

--- deploy.php
+++ deploy.php
@@ -13,7 +13,7 @@ set('repository', 'git@github.com:svbackend/my-art-lib.git');
 set('git_tty', true);
 
 // Shared files/dirs between deploys 
-add('shared_files', []);
+add('shared_files', ['docker-compose.prod.yml']);
 add('shared_dirs', []);
 
 // Writable dirs by web server 
@@ -24,11 +24,29 @@ host('159.89.14.82')
     ->user('svbackend')
     ->identityFile('~/.ssh/dokey')
     ->set('deploy_path', '~/{{application}}');
-    
+
+set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --ignore-platform-reqs --no-scripts');
+
+task('deploy:dotenv', function() {
+    $environment = run('cat {{deploy_path}}/shared/.env');
+    $dotenv = new \Symfony\Component\Dotenv\Dotenv();
+    $dotenv->populate($dotenv->parse($environment));
+})->desc('Load DotEnv values');
+after('deploy:shared', 'deploy:dotenv');
+
 // Tasks
 
 task('build', function () {
     run('cd {{release_path}} && build');
+});
+
+task('deploy:cache:clear', function () {
+    run('{{bin/console}} cache:clear --no-warmup --env=prod');
+});
+
+
+task('deploy:cache:warmup', function () {
+    run('{{bin/console}} cache:warmup --env=prod');
 });
 
 // [Optional] if deploy fails automatically unlock.
