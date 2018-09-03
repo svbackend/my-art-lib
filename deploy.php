a5ff95e9265603eebbd3a42bc0883928dca3d934
Valentin <svbackend22@gmail.com>
2018-09-03 10:09:57 +0300

    Publish production conf files for apache and supervisord

--- deploy.php
+++ deploy.php
@@ -7,6 +7,7 @@ require 'recipe/symfony4.php';
 set('application', 'mykino.top');
 set('default_timeout', 86400);
 set('timeout', 86200);
+set('ssh_multiplexing', true);
 
 // Project repository
 set('repository', 'git@github.com:svbackend/my-art-lib.git');
@@ -16,68 +17,25 @@ set('git_tty', true);
 set('http_user', 'www-data');
 
 // Shared files/dirs between deploys
-set('shared_files', ['docker-compose.prod.yml']);
-set('shared_dirs', []);
-set('writable_dirs', ['var']);
+add('shared_dirs', [
+    'public/f/movies',
+    'public/f/actors',
+]);
+add('writable_dirs', [
+    'public/f',
+    'public/f/movies',
+    'public/f/actors',
+]);
 
 // Hosts
-host('159.89.14.82')
-    ->user('svbackend')
+host('142.93.109.174')
+    ->user('deployer')
     ->identityFile('~/.ssh/dokey')
-    ->set('deploy_path', '~/{{application}}');
-
-set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --ignore-platform-reqs --no-scripts');
-
-// Tasks
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
-task('deploy:env', function () {
-    run('rm -f {{release_path}}/.env');
-    run('cp {{deploy_path}}/shared/.env {{release_path}}/.env');
-    run('cd {{release_path}} && chmod 755 -R var');
-});
-after('deploy:writable', 'deploy:env');
-
-task('deploy:var_dir', function () {
-    run('cd {{deploy_path}}/current && docker-compose down');
-    run('rm -f {{release_path}}/.env');
-    run('cp {{deploy_path}}/shared/.env {{release_path}}/.env');
-    run('cd {{release_path}} && chmod 755 -R var');
-});
-after('deploy:cache:warmup', 'deploy:var_dir');
-
-task('deploy:cache:clear', function () {
-    run('{{bin/console}} cache:clear --no-warmup --env=prod');
-});
-
-task('deploy:cache:warmup', function () {
-    run('{{bin/console}} cache:warmup --env=prod');
-});
+    ->set('deploy_path', '/var/www/mykino.top');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
-task('deploy:docker', function () {
-    run('cd {{deploy_path}}/current && docker-compose -f docker-compose.deploy.yml up -d');
-});
-after('deploy:symlink', 'deploy:docker');
-
-task('deploy:db', function () {
-    run('cd {{deploy_path}}/current && docker exec -i $(docker-compose ps -q app) php ./bin/console doctrine:migrations:migrate -n');
-});
-after('deploy:docker', 'deploy:db');
-
-task('deploy:production', function () {
-    run('cd {{deploy_path}}/current && docker-compose down');
-    run('cd {{deploy_path}}/current && ls -a');
-    run('cd {{deploy_path}}/current && docker ps | awk {\' print $1 \'} | tail -n+2 > tmp.txt; for line in $(cat tmp.txt); do docker kill $line; done; rm tmp.txt');
-    run('cd {{deploy_path}}/current && docker-compose -f docker-compose.prod.yml up -d');
-});
-after('deploy:db', 'deploy:production');
-
 // Migrate database before symlink new release.
-
-//before('deploy:symlink', 'database:migrate');
+before('deploy:symlink', 'database:migrate');
 
